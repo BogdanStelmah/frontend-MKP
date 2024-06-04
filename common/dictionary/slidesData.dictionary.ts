def4b5bbb87959037a45c5bef9e90d45536d5a20
Bohdan Dubynchenko <dubinchenkob@gmail.com>
2024-06-04 17:41:02 +0300

    Added new photos for the introductory page

--- common/dictionary/slidesData.dictionary.ts
+++ common/dictionary/slidesData.dictionary.ts
@@ -5,30 +5,30 @@ export const getSlidesData = () => [
     id: '1',
     title: i18n.t('introduction.slide1.title'),
     description: i18n.t('introduction.slide1.description'),
-    imageSource: require('../../assets/images/slide-1.png')
+    imageSource: require('../../assets/images/slide_1.jpg')
   },
   {
     id: '2',
     title: i18n.t('introduction.slide2.title'),
     description: i18n.t('introduction.slide2.description'),
-    imageSource: require('../../assets/images/slide-1.png')
+    imageSource: require('../../assets/images/slide_2.jpg')
   },
   {
     id: '3',
     title: i18n.t('introduction.slide3.title'),
     description: i18n.t('introduction.slide3.description'),
-    imageSource: require('../../assets/images/slide-1.png')
+    imageSource: require('../../assets/images/slide_3.jpg')
   },
   {
     id: '4',
     title: i18n.t('introduction.slide4.title'),
     description: i18n.t('introduction.slide4.description'),
-    imageSource: require('../../assets/images/slide-1.png')
+    imageSource: require('../../assets/images/slide_1.jpg')
   },
   {
     id: '5',
     title: i18n.t('introduction.slide5.title'),
     description: i18n.t('introduction.slide5.description'),
-    imageSource: require('../../assets/images/slide-1.png')
+    imageSource: require('../../assets/images/slide_4.jpg')
   }
 ];
